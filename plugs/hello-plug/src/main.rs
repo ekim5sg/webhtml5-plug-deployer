b0e84462b71fdc01801d5c6fec5e3901c6fae7e8
Michael Givens <owner@u-saw-it.com>
2026-02-20 07:24:43 -0600

    Convert to reusable Hostek Trunk deployer (workflow + templates + plugs)

---
+++ plugs/hello-plug/src/main.rs
@@ -0,0 +1,15 @@
+﻿use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <main style="font-family: system-ui; padding: 24px;">
+            <h1>{"Hello Plug"}</h1>
+            <p>{"If you can see this, your plug deployer pipeline works."}</p>
+        </main>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
