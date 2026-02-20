b6882e27a9bafb7594984df241658579553e0367
Michael Givens <owner@u-saw-it.com>
2026-02-20 09:16:26 -0600

    Add plug: this-is-cool (Trunk + Yew scaffold)

---
+++ plugs/this-is-cool/src/main.rs
@@ -0,0 +1,15 @@
+﻿use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <main style="font-family: system-ui; padding: 24px;">
+            <h1>{"This Is Cool ✅"}</h1>
+            <p>{"Your webhtml5 plug deployer pipeline is live."}</p>
+        </main>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
